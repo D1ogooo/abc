28d3bba8a50ff7c8914e7389615d6a12aabfe9e9
D1ogooo <diiogocosta11@gmail.com>
2024-05-24 20:17:03 -0300

    terminado

---
+++ src/component/Header/index.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import { HeaderDesktop } from '../HeaderDesktop'
+import { HeaderMobille } from '../HeaderMobbile'
+
+export function Header () {
+  const [state, setState] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const handleResize = () => {
+     setState(window.innerWidth)
+    }
+    window.addEventListener('resize', handleResize);
+   },[])
+
+  return ( <> { state > 1000 ? <HeaderDesktop /> : <HeaderMobille /> } </>)
+}
